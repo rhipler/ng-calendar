e28882b1c7b2a6c2b9003c86268e727bdac5b0b0
Rolf Hipler <rhipler@users.noreply.github.com>
2019-05-26 23:00:49 +0200

    show events

--- src/app/week-view/week-view.component.ts
+++ src/app/week-view/week-view.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import { CalendarEvent} from '../calendar-event';
+import {Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {CalendarEvent} from '../calendar-event';
+import {CalendarService} from '../calendar.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-week-view',
@@ -15,12 +17,21 @@ export class WeekViewComponent implements OnInit {
   private nextWeek: Date;
 
 
-  private days: {date: Date, events: CalendarEvent[]}[] = new Array();
+  private days: {date: Date, events: CalendarEvent[], wholeDayEvents: CalendarEvent[]}[] = Array();
 
   private hours: number[];
 
 
-  constructor(private route: ActivatedRoute) { }
+  public static sameday(date1: Date, date2: Date) {
+
+    return (date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDay() === date2.getDay() );
+  }
+
+
+
+
+  constructor(private route: ActivatedRoute, private calendarService: CalendarService, private sanitizer: DomSanitizer) { }
+
 
   ngOnInit() {
 
@@ -33,7 +44,7 @@ export class WeekViewComponent implements OnInit {
           this.weekdate = new Date(+params.get('year'), +params.get('month') - 1, +params.get('day'));
         }
 
-        // get the monday before weekdate, which is the beginning of this the week
+        // get the monday before weekdate, which is the beginning of this week
         const beginOfWeek = new Date(this.weekdate.getFullYear(), this.weekdate.getMonth(), this.weekdate.getDate() - (this.weekdate.getDay() + 6) % 7);
 
         this.prevWeek = new Date(beginOfWeek.getFullYear(), beginOfWeek.getMonth(), beginOfWeek.getDate() - 7);
@@ -41,17 +52,46 @@ export class WeekViewComponent implements OnInit {
 
         this.days = Array();
         for (let i = 0; i < 7; i++) {
-          this.days.push({date: new Date(beginOfWeek.getFullYear(), beginOfWeek.getMonth(), beginOfWeek.getDate() + i), events: null});
+          this.days.push({
+            date: new Date(beginOfWeek.getFullYear(), beginOfWeek.getMonth(), beginOfWeek.getDate() + i),
+            events: [],
+            wholeDayEvents: []
+          });
         }
+
+
+        // get the CalendarEvents for this week
+        this.calendarService.getEvents(beginOfWeek, this.nextWeek).subscribe(events => {
+          events.forEach(ev => {
+              const theDay = this.days.find(day => WeekViewComponent.sameday(day.date, ev.begin));
+              if (theDay) {
+                if (ev.wholeDay) {
+                  theDay.wholeDayEvents.push(ev);
+                } else {
+                  theDay.events.push(ev);
+                }
+              }
+            }
+          );
+        });
+
       }
     );
 
 
-    this.hours = Array();
+    this.hours = [];
     for (let i = 0; i < 24; i++) {
       this.hours.push(i);
     }
 
+  }
+
+
+  public eventPosition(event: CalendarEvent, i: number) {
+
+    const minutes = (event.end.valueOf() - event.begin.valueOf() ) / 60_000;
+
+    return this.sanitizer.bypassSecurityTrustStyle('height: ' + minutes * (40 / 60) + 'px; width: 25%; left: ' + (i * 25) + '%');
   }
 
 
