431145bff4cb3f62fdb79c50d79eb6f6c6d69d15
Rolf Hipler <rhipler@users.noreply.github.com>
2022-05-26 20:21:38 +0200

    update to ng 11

--- src/app/edit-event/edit-event.component.spec.ts
+++ src/app/edit-event/edit-event.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {EditEventComponent } from './edit-event.component';
 import {FormsModule} from '@angular/forms';
@@ -8,7 +8,7 @@ describe('EditEventComponent', () => {
   let component: EditEventComponent;
   let fixture: ComponentFixture<EditEventComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, FormsModule],
       declarations: [ EditEventComponent ]
--- src/app/month-view/month-view.component.spec.ts
+++ src/app/month-view/month-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {MonthViewComponent } from './month-view.component';
 import {RouterTestingModule} from '@angular/router/testing';
@@ -8,7 +8,7 @@ describe('MonthViewComponent', () => {
   let component: MonthViewComponent;
   let fixture: ComponentFixture<MonthViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule ],
       declarations: [ MonthViewComponent, DayDetailComponent]
